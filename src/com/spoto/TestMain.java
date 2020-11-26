bdee7cc4ec412217f2eab261e0767f0176a16754
spoto-dl <980816303@qq.com>
2020-11-26 16:50:49 +0800

    3 javagit-main

--- src/com/spoto/TestMain.java
+++ src/com/spoto/TestMain.java
@@ -5,6 +5,8 @@ public class TestMain {
     public static void main(String[] args) {
 
         System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
 
         System.out.println("hello world");
 
